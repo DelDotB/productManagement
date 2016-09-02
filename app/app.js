ba0c8c7eb73ae3d376c69a2ce7cdfc8fa12981c9
rocooley <rocooley@utmb.edu>
2016-09-02 14:40:42 -0500

    routing to multiple views - part 1 completed

--- app/app.js
+++ app/app.js
@@ -3,24 +3,35 @@
  */
 
 (function () {
-    "use strict";
-    var app =
-        angular
-            .module(
-                "productManagement", // module name
-                ["common.services", "ui.router", "productResourceMock"] // dependancies
-            );
+	"use strict";
+	var app =
+		angular
+			.module(
+				"productManagement", // module name
+				["common.services", "ui.router", "productResourceMock"] // dependancies
+			);
 
-    app.config(["$stateProvider",
-        function ($stateProvider) {
-          $stateProvider
-            // products
-              .state("productList", {
-                  url: "/products",
-                  templateUrl: "app/products/productListView.html",
-                  controller: "ProductListCtrl as vm"
-              });
-        }]
-    );
-    
+	app.config(["$stateProvider", "$urlRouterProvider",
+		function ($stateProvider, $urlRouterProvider) {
+			$urlRouterProvider.otherwise("/");
+
+			$stateProvider
+				.state("home", {
+					url: "/",
+					templateUrl: "app/welcomeView.html"
+				})
+			// products
+				.state("productList", {
+					url: "/products",
+					templateUrl: "app/products/productListView.html",
+					controller: "ProductListCtrl as vm"
+				})
+				.state("productEdit", {
+					url: "/products/edit/:productId",
+					templateUrl: "app/products/productEditView.html",
+					controller: "ProductEditCtrl as vm"
+				});
+		}]
+	);
+
 }());
